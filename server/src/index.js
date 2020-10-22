b4f2b91ee4228300cf0e9bf08b6bdf5cf5ee6daf
Tracey Kong <tzxkong11@gmail.com>
2020-10-21 20:48:13 -0400

    connected to firebase

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('../get-gud-key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://get-gud-fee28.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 const app = express();
+
+app.post('/test', async (req, res) => {
+    return res.json("Hello, world!");
+})
 
 app.listen(5000, () => console.log('App listening on port 5000.'));
