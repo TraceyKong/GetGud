8e1617fa149fdb6dabefec40ea83eaefd8dc3468
Tracey Kong <tzxkong11@gmail.com>
2020-10-21 20:52:52 -0400

    reeeeeee

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = require('../get-gud-key.json');
+const cors = require('cors');
 
+// Connect to firebase
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://get-gud-fee28.firebaseio.com"
 });
 
+// Database
 const db = admin.firestore();
 
+// Express app
 const app = express();
+
+app.use(express.json());    // Body parser
+app.use(cors());
 
 app.post('/test', async (req, res) => {
     return res.json("Hello, world!");
