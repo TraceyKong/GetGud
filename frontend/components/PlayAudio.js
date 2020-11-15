b50082e3eca0b09ec4c29b84c11e9c5b00da273f
davidy9000 <davidy.9000@gmail.com>
2020-11-15 05:05:51 -0500

    attempt with react-native-sound

--- frontend/components/PlayAudio.js
+++ frontend/components/PlayAudio.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import Sound from 'react-native-sound'
 import SoundPlayer from 'react-native-sound-player';
 
 class PlayAudio extends Component {
     handlePress(){
-        try {
-            // play the file tone.mp3
-            SoundPlayer.playSoundFile('tone', 'mp3')
-            // or play from url
-            SoundPlayer.playUrl('placeholder.url')
-          } catch (e) {
-            console.log(`cannot play the sound file`, e)
-          }
+        // try {
+        //     // play the file tone.mp3
+        //     SoundPlayer.playSoundFile('tone', 'mp3')
+        //     // or play from url
+        //     SoundPlayer.playUrl('placeholder.url')
+        //   } catch (e) {
+        //     console.log(`cannot play the sound file`, e)
+        //   }
+        const sound = new Sound('http://sounds.com/some-sound', null, (error) => {
+        if (error) {
+            // do something
+        }
+        
+        // play when loaded
+        sound.play();
+        });
     }
 
     render() {
