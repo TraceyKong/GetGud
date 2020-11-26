0b23519fb293a960acc2db9eb0d808a9ebc14c21
TraceyKong <tzxkong11@gmail.com>
2020-11-26 12:45:22 -0500

    used new base64 package

--- frontend/components/PlayAudio.js
+++ frontend/components/PlayAudio.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Button, View, Text } from 'react-native';
 import io from "socket.io-client";
-import { encode as btoa } from 'base-64';
+import { btoa } from 'js-base64';
 import { Audio } from 'expo-av';
 
 export default function PlayAudio() {
     const [socket] = useState(() => io("http://192.168.1.154:8080"));
-
     useEffect(() => {
         let chunks = [];
 
