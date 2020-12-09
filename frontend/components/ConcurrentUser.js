ba480fe73f8c6883e64742b23e1d8ae8c068807b
davidy9000 <davidy.9000@gmail.com>
2020-12-09 02:52:45 -0500

    manual retrieve online users

--- frontend/components/ConcurrentUser.js
+++ frontend/components/ConcurrentUser.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { View , Text } from "react-native";
+import { Button } from "@material-ui/core";
 
 export default function ConcurrentUser(props){
     const socket = props.socket;
 
     const [userCount, setUserCount] = useState('');
+    const [userNames, setUserNames] = useState([]);
     
     useEffect(() => {
         socket.on('counter', (data) =>{
-            setUserCount(data.user_count)
+            setUserCount(data.user_count);
+        });
+
+        socket.on('receiveNickname', (data) => {
+            const temp = data.usernames;
+            setUserNames(temp);
+            console.log('receive-user', userNames);
         })
+
     }, [socket]);
+
+    const handleClick = () => {
+        socket.emit('getNicknames');
+    }
 
     return(
         <View>
@@ -18,6 +31,21 @@ export default function ConcurrentUser(props){
                 style={{ fontSize: "50px" }}
             >
                 Online User Count: {userCount}
+            </Text>
+
+            <Button
+                onClick={handleClick}
+            >
+                Refresh Online Users
+            </Button>
+
+            <Text
+                style={{ fontSize: "50px" }}
+            >
+                List of Users: {"\n"}
+                    {userNames.map((user, key) => (
+                        <Text key={key}> {user} {"\n"}</Text>)
+                    )}
             </Text>
         </View>
     )
